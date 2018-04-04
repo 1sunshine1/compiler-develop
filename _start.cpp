7e48d84bd79da61af2ff72bb8f5eb8de995e0875
1sunshine1 <31052236+1sunshine1@users.noreply.github.com>
2018-04-04 16:14:30 +0800

    main之前的函数调用以及推出函数
    前面写过一篇北极之北之main函数之前，这篇文章其实解决的问题是main函数并不是第一个执行的函数，在main之前，函数的入口点是_start, _start会调用glibc里的__libc_start_main，main函数只是这个函数的入参。 在__libc_start_main中某一步，会执行main函数。这是上面一篇博文获取到的知识。
    
        前两天，CU的gongping11写了一个博文atexit函数和exit函数的理解，这篇博文提到了atexit可以注册退出函数，在main之后，执行注册退出函数。讲的非常的好，我们在gongping11的基础上再进一步。

---
+++ _start.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void preinit(int argc, char **argv, char **envp) {
+    printf("%s\n", __FUNCTION__);
+}
+
+void init(int argc, char **argv, char **envp) {
+    printf("%s\n", __FUNCTION__);
+}
+
+void fini() {
+    printf("%s\n", __FUNCTION__);
+}
+
+__attribute__((section(".init_array"))) typeof(init) *__init = init;
+__attribute__((section(".preinit_array"))) typeof(preinit) *__preinit = preinit;
+__attribute__((section(".fini_array"))) typeof(fini) *__fini = fini;
+
+void __attribute__((constructor)) constructor() {
+    printf("%s\n", __FUNCTION__);
+}
+
+void __attribute__((constructor)) constructor_2() {
+    printf("%s\n", __FUNCTION__);
+}
+
+void __attribute__((destructor)) destructor() {
+    printf("%s\n", __FUNCTION__);
+}
+
+void __attribute__((destructor)) destructor_2() {
+    printf("%s\n", __FUNCTION__);
+}
+void my_atexit() {
+    printf("%s\n", __FUNCTION__);
+}
+void my_atexit2() {
+    printf("%s\n", __FUNCTION__);
+}
+
+int main() {
+    printf("lalall\n");
+    atexit(my_atexit2);
+    atexit(my_atexit);
+    printf("%s\n", __FUNCTION__);
+    printf("i am here\n");
+}
+  
